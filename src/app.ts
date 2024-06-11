9edd8913f44802f6d1979ca7c4d7a866b2224b37
Max Vigueras <mvt.2000@hotmail.com>
2024-06-11 12:42:55 -0600

    TypeScript: Migration to TS

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+const heroes = [
+    {
+        id: 1,
+        name: 'Ironman',
+        owner: 'Marvel'
+    },
+    {
+        id: 2,
+        name: 'Spiderman',
+        owner: 'Marvel'
+    },
+    {
+        id: 3,
+        name: 'Thor',
+        owner: 'Marvel'
+    },        
+]
+
+
+
+const findHeroById = ( id: number) => {
+    return heroes.find( (hero) => hero.id === id );
+}
+
+const hero = findHeroById( 4 );
+
+console.log( hero?.name ?? 'No hero found!!')
